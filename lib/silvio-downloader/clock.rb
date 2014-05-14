302c76d7c5c338e771e298c0ba98a239651e519c
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2014-05-14 16:10:06 -0300

    Fix Realod config file every run

--- lib/silvio-downloader/clock.rb
+++ lib/silvio-downloader/clock.rb
@@ -7,7 +7,7 @@ require 'rtransmission'
 
 include Clockwork
 
-@config = SilvioDownloader::Configuration.new('config/silvio-downloader.json')
+@config = read_config
 @logger = Logger.new('log/silvio_downloader.log')
 
 @session = RTransmission::Client.session(
@@ -18,6 +18,7 @@ include Clockwork
     )
 
 handler do |job|
+  @config = read_config
   @logger.info('Loading config/silvio-downloader.json')
   @config.shows.each do |show|
     next if download_next_episode(show, show.find_best_link_episode)
@@ -28,6 +29,10 @@ handler do |job|
 end
 
 every(@config.hour_interval.hours, 'check_new_torrents.job')
+
+def read_config
+  SilvioDownloader::Configuration.new('config/silvio-downloader.json')
+end
 
 def download_next_episode(show, link, update_to_next_seasson = false)
   return false if link.nil?
